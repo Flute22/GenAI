f75e14c5b2a52a0518c6bcc161ca711ff12ea47b
khushalsinhmar <shimarsabb@gmail.com>
2026-01-04 01:09:16 +0530

    building a chat-bot

--- first_class/embedding.py
+++ first_class/embedding.py
@@ -1,1 +1,15 @@
+import os
 from dotenv import load_dotenv # pyright: ignore[reportMissingImports]
+from google import genai
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+prompt = input("Enter your prompt: ")
+
+response = client.models.generate_content(
+    model = 'gemini-3-flash-preview',
+    contents = {prompt}
+)
+
+print("Response:", response.text)
